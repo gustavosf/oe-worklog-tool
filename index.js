4b5734cbce1d5694d13f2ce7f19a67cbb2e49d71
Gustavo Seganfredo <gustavosf@gmail.com>
2019-02-28 20:55:56 -0300

    Adding some sprint and issue listing commands

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const JiraClient = require("jira-connector")
 const program = require("commander")
 const config = require("./config.json")
+const Table = require("cli-table")
 
 const jira = new JiraClient(config.credentials)
 
@@ -17,7 +18,7 @@ program
     .usage("COMMAND [options]")
 
 program 
-    .command("log <alias> [time]")
+    .command("log add <alias> [time]")
     .option("-c, --comment <comment>", "Add a comment into the log")
     .action((alias, time, options) => {
         let conf = config.aliases[alias]
@@ -35,13 +36,69 @@ program
     })
 
 program 
-    .command("rmlog <issueKey> <worklogId>")
+    .command("log rm <issueKey> <worklogId>")
     .action((issueKey, worklogId) => {
         jira.issue.deleteWorkLog({
             issueKey, worklogId
         }).then(ret => {
             console.log(`Worklog ${worklogId} removed from issue ${issueKey}`)
         }).catch(logError)
+    })
+
+program
+    .command("issues list")
+    .action(() => {
+        jira.search.search({
+            jql: "assignee=currentUser() and (project=PA or project=PAA)"
+        }).then(ret => {
+            let table = new Table({
+                head: ["Key", "Summary"]
+            })
+
+            for (let issue of ret.issues) {
+                table.push([issue.key, issue.fields.summary])
+            } 
+            console.log(table.toString()) 
+        })
+        .catch(ret => { console.log(ret) })
+    })
+
+program
+    .command("sprint")
+    .option("--open", "Filter out closed issues")
+    .option("--last", "Show the last sprint")
+    .option("--next", "Show the next sprint")
+    .option("--mine", "Show only issues assigned to you")
+    .action((opt) => {
+        jira.board.getSprintsForBoard({ boardId: 334, state: opt.last ? "closed" : (opt.next ? "future" : "active") }).then(s => {
+            let sprint = opt.future ? s.values.shift() : s.values.pop()
+            let jql = []
+            if (opt.open) jql.push("statusCategory != done")
+            if (opt.mine) jql.push("assignee = currentUser()")
+            jira.sprint.getSprintIssues({
+                sprintId: sprint.id,
+                maxResults: 9999,
+                jql: jql.join(" AND ")
+            }).then(s => {
+                let table = new Table({
+                    head: ["Key", "Summary", "Assignee", "Points", "State"]
+                })
+                let points = 0
+                for (let issue of s.issues) {
+                    table.push([
+                        issue.key,
+                        issue.fields.summary,
+                        issue.fields.assignee.name,
+                        issue.fields.customfield_10105 || 0,
+                        issue.fields.status.name
+                    ])
+                    points += issue.fields.customfield_10105 || 0
+                }
+                table.push(["", "", "", points, ""])
+                console.log(table.toString())
+            })
+        })
+        
     })
 
 program.parse(process.argv)
